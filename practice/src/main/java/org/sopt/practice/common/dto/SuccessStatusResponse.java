a020869dd6b70b583d45802a5a4754baf313e188
minwoo0419 <ghdl464@naver.com>
2024-05-02 17:43:57 +0900

    response 반환값 수정

--- practice/src/main/java/org/sopt/practice/common/dto/SuccessStatusResponse.java
+++ practice/src/main/java/org/sopt/practice/common/dto/SuccessStatusResponse.java
@@ -2,9 +2,10 @@ package org.sopt.practice.common.dto;
 
 public record SuccessStatusResponse (
         int status,
+        Object data,
         String message
 ){
-    public static SuccessStatusResponse of(SuccessMessage successMessage){
-        return new SuccessStatusResponse(successMessage.getStatus(), successMessage.getMessage());
+    public static SuccessStatusResponse of(SuccessMessage successMessage, Object data){
+        return new SuccessStatusResponse(successMessage.getStatus(), data, successMessage.getMessage());
     }
 }
--- practice/src/main/java/org/sopt/practice/controller/PostController.java
+++ practice/src/main/java/org/sopt/practice/controller/PostController.java
@@ -6,8 +6,6 @@ import org.sopt.practice.common.dto.SuccessMessage;
 import org.sopt.practice.common.dto.SuccessStatusResponse;
 import org.sopt.practice.service.PostService;
 import org.sopt.practice.service.dto.PostCreateRequest;
-import org.sopt.practice.service.dto.PostFindDto;
-import org.sopt.practice.service.dto.PostListDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,20 +24,20 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED).header(
                 "Location",
                 postService.create(blogId, memberId, postCreateRequest))
-                .body(SuccessStatusResponse.of(SuccessMessage.POST_CREATE_SUCCESS));
+                .body(SuccessStatusResponse.of(SuccessMessage.POST_CREATE_SUCCESS, null));
     }
 
     @GetMapping("/post")
-    public ResponseEntity<PostListDto> findAllByBlogId(
+    public ResponseEntity<SuccessStatusResponse> findAllByBlogId(
             @RequestHeader Long blogId
     ){
-        return ResponseEntity.ok(postService.findAllByBlogId(blogId));
+        return ResponseEntity.ok(SuccessStatusResponse.of(SuccessMessage.POST_GET_SUCCESS ,postService.findAllByBlogId(blogId)));
     }
 
     @GetMapping("/post/{postId}")
-    public ResponseEntity<PostFindDto> findById(
+    public ResponseEntity<SuccessStatusResponse> findById(
             @PathVariable Long postId
     ){
-        return ResponseEntity.ok(postService.findPost(postId));
+        return ResponseEntity.ok(SuccessStatusResponse.of(SuccessMessage.POST_GET_SUCCESS, postService.findPost(postId)));
     }
 }
